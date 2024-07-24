733867bff6c5e30f4d250f18af0d109b80d2ae84
Mishaaa2024 <mishafatimaali2013@gmail.com>
2024-07-24 14:24:51 +0500

    first commit

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,139 +1,178 @@
 import {
-  Box,
-  Image,
-  Flex,
-  GridItem,
-  
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
-  Text,
-  useDisclosure,
-  Avatar,
-  Divider,
-  VStack,
+	Avatar,
+	Button,
+	Divider,
+	Flex,
+	GridItem,
+	Image,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalOverlay,
+	Text,
+	VStack,
+	useDisclosure,
 } from "@chakra-ui/react";
-import React from 'react'
 import { AiFillHeart } from "react-icons/ai";
 import { FaComment } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import Comment from '../Comments/Comment'
-import PostFooter from '../FeedPosts/PostFooter'
+import PostFooter from "../FeedPosts/PostFooter";
+import useUserProfileStore from "../../store/userProfileStore";
+import useAuthStore from "../../store/authStore";
+import useShowToast from "../../hooks/useShowToast";
+import { useState } from "react";
+import { deleteObject, ref } from "firebase/storage";
+import { firestore, storage } from "../../firebase/firebase";
+import { arrayRemove, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import usePostStore from "../../store/postStore";
+import Caption from "../Comments/Caption";
 
-const ProfilePost = ({ img }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  return (
-    <>
-      <GridItem
-        cursor={"pointer"}
-        borderRadius={4}
-        overflow={"hidden"}
-        border={"1px solid"}
-        borderColor={"whiteAlpha.300"}
-        position={"relative"}
-        aspectRatio={1 / 1}
-        onClick={onOpen}
-      >
-        <Flex
-          opacity={0}
-          _hover={{ opacity: 1 }}
-          position={"absolute"}
-          top={0}
-          left={0}
-          right={0}
-          bottom={0}
-          bg={"blackAlpha.700"}
-          transition={"all 0.3s ease"}
-          zIndex={1}
-          justifyContent={"center"}
-        >
+const ProfilePost = ({ post }) => {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const userProfile = useUserProfileStore((state) => state.userProfile);
+	const authUser = useAuthStore((state) => state.user);
+	const showToast = useShowToast();
+	const [isDeleting, setIsDeleting] = useState(false);
+	const deletePost = usePostStore((state) => state.deletePost);
+	const decrementPostsCount = useUserProfileStore((state) => state.deletePost);
 
-          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
-            <Flex>
-              <AiFillHeart size={20} />
-              <Text fontWeight={"bold"} ml={2}>
-                7
-              </Text>
-            </Flex>
+	const handleDeletePost = async () => {
+		if (!window.confirm("Are you sure you want to delete this post?")) return;
+		if (isDeleting) return;
 
-            <Flex>
-              <FaComment size={20} />
-              <Text fontWeight={"bold"} ml={2}>
-                5
-              </Text>
-            </Flex>
-          </Flex>
+		try {
+			const imageRef = ref(storage, `posts/${post.id}`);
+			await deleteObject(imageRef);
+			const userRef = doc(firestore, "users", authUser.uid);
+			await deleteDoc(doc(firestore, "posts", post.id));
 
-        </Flex>
-       
-        <Image src={img} alt='profile-post' w={'100%'} h={'100%'} objectFit={'cover'} objectPosition={'right'} />
-      </GridItem>
+			await updateDoc(userRef, {
+				posts: arrayRemove(post.id),
+			});
 
-      <Modal isOpen={isOpen} onClose={onClose}
-        isCentered={true}
-        size={{ base: "3xl", md: "5xl" }}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          
-          <ModalCloseButton />
-          <ModalBody bg={'black'} pb={5}>
-            <Flex gap={4} w={{ base: '90%', sm: "70%" }} mx={"auto"}>
-              <Box borderRadius={4}
-                overflow={"hidden"}
-                border={"1px solid"}
-                borderColor={"whiteAlpha.300"}
-                flex={1.5}
-              >
-                <Image src={img} alt="profile post" w={'100%'} h={'100%'} objectFit={'cover'} />
-              </Box>
-              <Flex flex={1} flexDir={"column"}px={10}display={{base:"none",md:"flex"}}>
-                <Flex alignItems={'center'}justifyContent={'space-between'}>
-                 <Flex alignItems={'center'}justifyContent={'space-between'}>
-                    <Avatar src="/download (1).png"size={'sm'} name="Mysha Fatima"/>
-                    <Text fontWeight={'bold'} fontSize={12} ml={2}>
-                       Mysha Fatima
-                    </Text>
-                 </Flex>
-                 <Box _hover={{bg:"whiteAlpha.300",color:"red.600"}}borderRadius={4}p={1}>
-                  <MdDelete size={20} cursor="pointer"/>
-                 </Box>
-                 </Flex>
-                 <Divider my={4}bg={'gray.500'}/>
-                 <VStack w={'full'}alignItems={'start'}maxH={'350px'}overflowY={'auto'}>
-                   <Comment 
-                   CreatedAt="1d ago"
-                   username="Bestie"
-                   profilePic="/img4.png"
-                   text={"Dummy images from unsplash"}
-                   />
-                    <Comment 
-                   CreatedAt="1d ago"
-                   username="HamnaAhmed"
-                   profilePic="/img1.png"
-                   text={"Dummy images from unsplash"}
-                   />
-                    <Comment 
-                   CreatedAt="1d ago"
-                   username="RiyaShanaya"
-                   profilePic="/img2.png"
-                   text={"Dummy images from unsplash"}
-                   />
-                 </VStack>
-                 <Divider my={4}bg={'gray.800'}/>
-                 <PostFooter isProfilePage={true}/>
-              </Flex>  
-            </Flex>
-          </ModalBody>
+			deletePost(post.id);
+			decrementPostsCount(post.id);
+			showToast("Success", "Post deleted successfully", "success");
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		} finally {
+			setIsDeleting(false);
+		}
+	};
 
+	return (
+		<>
+			<GridItem
+				cursor={"pointer"}
+				borderRadius={4}
+				overflow={"hidden"}
+				border={"1px solid"}
+				borderColor={"whiteAlpha.300"}
+				position={"relative"}
+				aspectRatio={1 / 1}
+				onClick={onOpen}
+			>
+				<Flex
+					opacity={0}
+					_hover={{ opacity: 1 }}
+					position={"absolute"}
+					top={0}
+					left={0}
+					right={0}
+					bottom={0}
+					bg={"blackAlpha.700"}
+					transition={"all 0.3s ease"}
+					zIndex={1}
+					justifyContent={"center"}
+				>
+					<Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+						<Flex>
+							<AiFillHeart size={20} />
+							<Text fontWeight={"bold"} ml={2}>
+								{post.likes.length}
+							</Text>
+						</Flex>
 
-        </ModalContent>
-      </Modal>
+						<Flex>
+							<FaComment size={20} />
+							<Text fontWeight={"bold"} ml={2}>
+								{post.comments.length}
+							</Text>
+						</Flex>
+					</Flex>
+				</Flex>
 
-    </>
-  )
-}
+				<Image src={post.imageURL} alt='profile post' w={"100%"} h={"100%"} objectFit={"cover"} />
+			</GridItem>
 
-export default ProfilePost
+			<Modal isOpen={isOpen} onClose={onClose} isCentered={true} size={{ base: "3xl", md: "5xl" }}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalCloseButton />
+					<ModalBody bg={"black"} pb={5}>
+						<Flex
+							gap='4'
+							w={{ base: "90%", sm: "70%", md: "full" }}
+							mx={"auto"}
+							maxH={"90vh"}
+							minH={"50vh"}
+						>
+							<Flex
+								borderRadius={4}
+								overflow={"hidden"}
+								border={"1px solid"}
+								borderColor={"whiteAlpha.300"}
+								flex={1.5}
+								justifyContent={"center"}
+								alignItems={"center"}
+							>
+								<Image src={post.imageURL} alt='profile post' />
+							</Flex>
+							<Flex flex={1} flexDir={"column"} px={10} display={{ base: "none", md: "flex" }}>
+								<Flex alignItems={"center"} justifyContent={"space-between"}>
+									<Flex alignItems={"center"} gap={4}>
+										<Avatar src={userProfile.profilePicURL} size={"sm"} name='As a Programmer' />
+										<Text fontWeight={"bold"} fontSize={12}>
+											{userProfile.username}
+										</Text>
+									</Flex>
+
+									{authUser?.uid === userProfile.uid && (
+										<Button
+											size={"sm"}
+											bg={"transparent"}
+											_hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+											borderRadius={4}
+											p={1}
+											onClick={handleDeletePost}
+											isLoading={isDeleting}
+										>
+											<MdDelete size={20} cursor='pointer' />
+										</Button>
+									)}
+								</Flex>
+								<Divider my={4} bg={"gray.500"} />
+
+								<VStack w='full' alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
+									{/* CAPTION */}
+									{post.caption && <Caption post={post} />}
+									{/* COMMENTS */}
+									{post.comments.map((comment) => (
+										<Comment key={comment.id} comment={comment} />
+									))}
+								</VStack>
+								<Divider my={4} bg={"gray.8000"} />
+
+								<PostFooter isProfilePage={true} post={post} />
+							</Flex>
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export default ProfilePost;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,146 +1,80 @@
-import { Avatar, Box, Flex, Icon, Link, Tooltip } from '@chakra-ui/react'
-import React from 'react'
-import { Link as RouterLink } from 'react-router-dom'
-import { CreatePostLogo, InstagramLogo, InstagramMobileLogo, NotificationsLogo, SearchLogo } from '../../assets/constants'
-import { AiFillHome } from 'react-icons/ai';
-import {BiLogOut} from 'react-icons/bi'
+import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
+
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
+import SidebarItems from "./SidebarItems";
 
 const Sidebar = () => {
-				const SidebarItems = [
-								{
-												icon: <AiFillHome size={25} />,
-												text: "Home",
-												link: "/",
-								},
-								{
-												icon: <SearchLogo />,
-												text: "Search",
+	const { handleLogout, isLoggingOut } = useLogout();
+	return (
+		<Box
+			height={"100vh"}
+			borderRight={"1px solid"}
+			borderColor={"whiteAlpha.300"}
+			py={8}
+			position={"sticky"}
+			top={0}
+			left={0}
+			px={{ base: 2, md: 4 }}
+		>
+			<Flex direction={"column"} gap={10} w='full' height={"full"}>
+				<Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor='pointer'>
+					<InstagramLogo />
+				</Link>
+				<Link
+					to={"/"}
+					as={RouterLink}
+					p={2}
+					display={{ base: "block", md: "none" }}
+					borderRadius={6}
+					_hover={{
+						bg: "whiteAlpha.200",
+					}}
+					w={10}
+					cursor='pointer'
+				>
+					<InstagramMobileLogo />
+				</Link>
+				<Flex direction={"column"} gap={5} cursor={"pointer"}>
+					<SidebarItems />
+				</Flex>
 
-								},
-								{
-												icon: <NotificationsLogo />,
-												text: "Notifications",
+				{/* LOGOUT */}
+				<Tooltip
+					hasArrow
+					label={"Logout"}
+					placement='right'
+					ml={1}
+					openDelay={500}
+					display={{ base: "block", md: "none" }}
+				>
+					<Flex
+						onClick={handleLogout}
+						alignItems={"center"}
+						gap={4}
+						_hover={{ bg: "whiteAlpha.400" }}
+						borderRadius={6}
+						p={2}
+						w={{ base: 10, md: "full" }}
+						mt={"auto"}
+						justifyContent={{ base: "center", md: "flex-start" }}
+					>
+						<BiLogOut size={25} />
+						<Button
+							display={{ base: "none", md: "block" }}
+							variant={"ghost"}
+							_hover={{ bg: "transparent" }}
+							isLoading={isLoggingOut}
+						>
+							Logout
+						</Button>
+					</Flex>
+				</Tooltip>
+			</Flex>
+		</Box>
+	);
+};
 
-								},
-								{
-												icon: <CreatePostLogo />,
-												text: "Create",
-
-								},
-								{
-												icon: <Avatar size={'sm'} name='Burak Orkmez' src='/download (1).png' />,
-												text: "Profile",
-												link: "/:username",
-								},
-
-				]
-				return (
-								<Box
-												height={'100vh'}
-												borderRight={'1px solid'}
-												borderColor={'whiteAlpha.300'}
-												py={8}
-												position={'sticky'}
-												top={0}
-												left={0}
-												px={{ base: 2, md: 4 }}
-								>
-
-												<Flex direction={'column'} gap={10} w={'full'} h={'full'}>
-
-																<Link to={"/"} as={RouterLink} pl={2} display={{ base: "none", md: "block" }} cursor={'pointer'}>
-																				<InstagramLogo />
-																</Link>
-
-																<Link to={"/"} as={RouterLink} p={2} display={{ base: "block", md: "none" }} cursor={'pointer'} borderRadius={6}
-																				_hover={{
-																								bg: 'whiteAlpha.200',
-																				}
-
-																				}
-																				w={10}
-																>
-																				<InstagramMobileLogo />
-																</Link>
-																<Flex direction={'column'} gap={5} cursor={'pointer'}>
-																				{SidebarItems.map((item, index) => (
-																												<Tooltip
-                            key={index}
-																												hasArrow
-																												label={item.text}
-																												placement='right'
-																												ml={1}
-																												openDelay={500}
-																												display={{ base: "block", md: "none" }}
-
-
-																								>
-																												<Link
-																																display={'flex'}
-																																alignItems={'center'}
-																																gap={4}
-																																_hover={{ bg: "whiteAlpha.400" }}
-																																borderRadius={6}
-																																padding={2}
-																																w={{base:10,md:'full'}}
-																																justifyContent={{ base: "center", md: "flex-start" }}
-																																to={item.link || null}
-																																as={RouterLink}
-																																
-																												>
-																																{item.icon}
-																																<Box display={{ base: "none", md: "block" }}>
-																																				{item.text}
-																																</Box>
-																												</Link>
-																								</Tooltip>
-
-																				))}
-																</Flex>
-
-		
-												</Flex>
-						
-
-
-{/* log out */}
-
-
-<Tooltip
-                           
-                           hasArrow
-                           label={'LogOut'}
-                           placement='right'
-                           ml={1}
-                           openDelay={500}
-                           display={{ base: "block", md: "none" }}
-
-
-                       >
-                           <Link
-                               display={'flex'}
-                               alignItems={'center'}
-                               gap={4}
-                               _hover={{ bg: "whiteAlpha.400" }}
-                               borderRadius={6}
-                               padding={2}
-                               w={{base:10,md:'full'}}
-                               justifyContent={{ base: "center", md: "flex-start" }}
-                               to={'/auth'}
-							   mt={'auto'}
-							   
-                               as={RouterLink}
-                               
-                           >
-                               <BiLogOut size={25} />
-                               <Box display={{ base: "none", md: "block" }}>
-                                   LogOut
-                               </Box>
-                           </Link>
-                       </Tooltip>
-
-								</Box>
-				)
-}
-
-export default Sidebar
+export default Sidebar;
